a075fe6e4ab5cadefe3f467ac91dbadbff618fa3
josn7 <josenilsonsilva123@gmail.com>
2025-03-21 00:38:15 -0300

    week 2 activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2005;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Apple";
+        job2._startYear = 2009;
+        job2._endYear = 2011;
+
+        Resume myResume = new Resume();
+        myResume._name = "Joseph Smith";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
